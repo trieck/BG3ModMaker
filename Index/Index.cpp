07d302d53495ff15f63b4ad1ab922945d095c262
Thomas A. Rieck <trieck@gmail.com>
2025-10-05 14:48:04 -0400

    Bitmaps.

--- Index/Index.cpp
+++ Index/Index.cpp
@@ -1,25 +1,32 @@
 #include "stdafx.h"
-
-#include <xapian.h>
-
 #include "Indexer.h"
 #include "Timer.h"
 
+#include <xapian.h>
+
 int main(int argc, char* argv[])
 {
-    if (argc != 3) {
-        std::cerr << "usage: " << argv[0] << " {package-path} {db-path}\n";
+    if (argc < 3) {
+        std::cerr << "usage: " << argv[0] << " {package-path} {index-path} <overwrite: true/false>\n";
         return 1;
     }
 
     std::string pakPath = argv[1];
     std::string dbPath = argv[2];
 
+    auto overwrite = false;
+
+    if (argc >= 4) {
+        std::string overwriteStr = argv[3];
+        std::ranges::transform(overwriteStr, overwriteStr.begin(), tolower);
+        overwrite = (overwriteStr == "true" || overwriteStr == "1" || overwriteStr == "yes");
+    }
+
     try {
         Timer timer;
 
         Indexer indexer;
-        indexer.index(argv[1], argv[2]);
+        indexer.index(argv[1], argv[2], overwrite);
 
         std::cout << "   Indexing took: " << timer.str() << std::endl;
 
